3861f751b345b1e027c5abb09a8cb26218136fac
matlock <jfflores90@gmail.com>
2015-04-03 17:53:16 -0300

    Se creó el metodo 'CharCount' y se hicieron pasar 4 test'

--- char_counter/model/char_counter.rb
+++ char_counter/model/char_counter.rb
@@ -1,3 +1,11 @@
 class CharCounter
 
+    def self.count(string)
+		hash = Hash.new
+		string.each_char{|c| hash[c] = string.count(c)
+                                                   
+                        }
+        return hash
+	end
+
 end
