49628a4d678ca5fcf698be476c670cb4fa23b0b7
matlock <jfflores90@gmail.com>
2015-05-27 23:06:41 -0300

    cracion del feature Disparo, dado un tablero y un barco largo.. Disparo a una coordenada incorrecta y obtengo agua

--- batalla_naval/features/step_definitions/disparo_step.rb
+++ batalla_naval/features/step_definitions/disparo_step.rb
@@ -3,40 +3,30 @@ require_relative '../../app/models/Barco.rb'
 
 
 Given(/^a board with dimension "(.*?)" x "(.*?)"$/) do |x, y|
-  	@tablero = Tablero.new(x.to_i,y.to_i)
+  	#@tablero = Tablero.new(x.to_i,y.to_i)
+  visit '/mipagina'
+  fill_in(:ancho, :with => x)
+  fill_in(:alto, :with => y)
+  click_button "crearTablero"
 end
 
 Given(/^a large ship in position  "(.*?)"$/) do |coords|
-  @barco_largo = BarcoLargo.new("Alpha",2)
-  @tablero.ponerBarcoEn(coords,@barco_largo)
+  #@barco_largo = BarcoLargo.new("Alpha",2)
+  #@tablero.ponerBarcoEn(coords,@barco_largo)
+  fill_in(:coordenadas, :with => coords)
+  fill_in(:nombreBarco, :with => "Delta")
+  fill_in(:tamanio, :with => "2")
+  click_button "crearBarco"
 end
 
 Given(/^I shoot to position "(.*?)"$/) do |coord|
-  @resultado = @tablero.dispararEn(coord)
+  #@resultado = @tablero.dispararEn(coord)
+  fill_in(:coordDisparo,:with => coord)
+  click_button "disparo"
 end
 
 
 Then(/^I get "(.*?)"$/) do |water|
-  expect(@resultado).to eq water
-end
-
-
-Given(/^I shoot to position "(.*?)" and assert the hit$/) do |coord|
- 	@resultado2 = @tablero.dispararEn(coord)
-end
-
-
-Then(/^I got "(.*?)"$/) do |hit|
-  expect(@resultado2).to eq hit
-end
-
-Given(/^I shoot the positions "(.*?)" "(.*?)"$/) do |coord1, coord2|
-  @tablero.dispararEn(coord1)
-  @resultado = @tablero.dispararEn(coord2)
-end
-
-
-
-Then(/^this time I got "(.*?)"$/) do |sink|
-  expect(@resultado).to eq sink
+  #expect(@resultado).to eq water
+  page.should have_content(water)
 end
