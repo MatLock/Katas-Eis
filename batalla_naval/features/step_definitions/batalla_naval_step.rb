bf9924c9cbba13d32506c43fb99a30546c938579
matlock <jfflores90@gmail.com>
2015-05-27 22:51:35 -0300

    correccion de los escenarios debido a que se coloco un step incorrecto

--- batalla_naval/features/step_definitions/batalla_naval_step.rb
+++ batalla_naval/features/step_definitions/batalla_naval_step.rb
@@ -4,13 +4,20 @@ require_relative '../../app/models/Exceptions.rb'
 require_relative '../../app/models/BarcoYaExistenteException.rb'
 
 
-
-Given(/^a board with dimension "(.*?)" x "(.*?)"$/) do |x, y|
+Given(/^a board with dimensions "(.*?)" x "(.*?)"$/) do |x, y|
     #@tablero = Tablero.new(x.to_i,y.to_i)
   visit '/mipagina'
   fill_in(:ancho, :with => x)
   fill_in(:alto, :with => y)
   click_button "crearTablero"
+end
+
+Given(/^I create a small ship in position "(.*?)"$/) do |coord|
+  #@tablero.ponerBarcoEn(coord,BarcoChico.new("Foxtrox"))
+  fill_in(:coordenadas, :with => coord)
+  fill_in(:nombreBarco, :with => "Foxtrox")
+  fill_in(:tamanio, :with => "1")
+  click_button "crearBarco"
 end
 
 
@@ -20,3 +27,5 @@ Then(/^position "(.*?)" is not empty$/) do |coord|
   click_button "pregunta"
   page.should have_content('false')
 end
+
+
